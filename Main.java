f4f56433029a246f07d8a26b329e2e1fd5776927
k00zaK1 <127319229+k00zaK1@users.noreply.github.com>
2023-04-05 12:38:54 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int j =5;
+    int k = 6;
+    for (int i=0; i<j;i++){
+    for (int t=0;t<k-i-1;t++){
+      System.out.print("*");
+    }
+  System.out.println(" ");
+    }
+  }
+}
